b8d47917fa89c7d7850462b92a454fe46280a6dd
flamerten <86600558+flamerten@users.noreply.github.com>
2021-07-04 18:51:09 +0800

    Add files via upload

---
+++ instabot.py
@@ -0,0 +1,109 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from pass_gen import *
+
+
+def login(user):
+    old_pass = return_latest_pass()[1]
+    username = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.NAME  ,
+                                        "username")))
+    username.send_keys(user)
+
+    password = driver.find_element_by_name("password")
+    password.send_keys(old_pass)    
+
+    try:
+        login = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button')
+        login.click()
+    except:
+        driver.quit()
+        print("login unsuccessful")
+
+def popups():
+    try:
+        info = WebDriverWait(driver,10).until(
+            EC.presence_of_element_located((By.XPATH  ,
+                                            '//*[@id="react-root"]/section/main/div/div/div/div/button')))
+        info.click()
+    except:
+        pass
+
+    try:
+        notif = WebDriverWait(driver,10).until(
+            EC.presence_of_element_located((By.XPATH  ,
+                                            '/html/body/div[4]/div/div/div/div[3]/button[1]')))
+        notif.click()
+    except:
+        pass
+
+def navigate():
+    profile = WebDriverWait(driver,10).until(
+            EC.presence_of_element_located((By.XPATH  ,
+                                            '//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/span')))
+    profile.click()
+
+
+    settings = WebDriverWait(driver,10).until(
+            EC.presence_of_element_located((By.XPATH  ,
+                                            '//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div[2]/div[2]/a[3]')))
+
+    settings.click()
+
+    change_pass = WebDriverWait(driver,10).until(
+            EC.presence_of_element_located((By.LINK_TEXT  ,
+                                            'Change Password')))
+
+    change_pass.click()
+
+def enter():
+    old_box = WebDriverWait(driver,10).until(
+            EC.presence_of_element_located((By.NAME  ,
+                                            'cppOldPassword')))
+
+    new_box1 = driver.find_element_by_name("cppNewPassword")
+    new_box2 = driver.find_element_by_name("cppConfirmPassword")
+
+    old_pass = return_latest_pass()[1]
+    new_pass = generate()
+
+    add_entry(new_pass)
+
+    old_box.send_keys(old_pass)
+    new_box1.send_keys(new_pass)
+    new_box2.send_keys(new_pass)
+
+    confirm = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/form/div[4]/div/div/button')
+
+    confirm.click()
+    time.sleep(10)
+    
+    if old_box.get_attribute("value") == "":
+        print("Password changed successfully")
+    else:
+        print("Password change failed")
+
+
+    
+
+PATH = 'C:\Program Files (x86)\chromedriver.exe'
+
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.instagram.com/")
+login('flamerten')
+popups()
+navigate()
+enter()
+#driver.quit()
+
+
+
+
+
---
+++ pass_gen.py
@@ -0,0 +1,67 @@
+import csv
+from random_word import RandomWords
+import random
+from datetime import date
+
+def read_csv(csvfilename):
+    """
+    Reads a csv file and returns a list of list
+    containing rows in the csv file and its entries.
+    """
+    rows = []
+
+    with open(csvfilename) as csvfile:
+        file_reader = csv.reader(csvfile)
+        for row in file_reader:
+            rows.append(row)
+    return rows
+
+def clear_csv(fname):
+    csv_file = open('pwds.csv','w',newline = "" )
+    csv_file.close()
+
+def add_entry(new_entry):
+    hx = read_csv('pwds.csv')
+
+    csv_file = open('pwds.csv','w',newline = "" )
+    csv_writer = csv.writer(csv_file)
+
+    for entry in hx:
+        csv_writer.writerow(entry)
+        
+    csv_writer.writerow([date_gen(),new_entry])
+
+    csv_file.close()
+
+def generate():
+    r = RandomWords()
+    f1 = r.get_random_word(minLength = 3)
+    f2 = r.get_random_word(minLength = 3)
+    num = random.randint(100,999)
+
+    return f1+f2+str(num)
+
+def date_gen():
+    today = date.today()
+    return today.strftime("%d/%m/%Y")
+
+def get_new_pass():
+    new_pass = generate()
+    add_entry('pwds.csv',new_pass)
+    print("New password added")
+
+def print_hx():
+    hx = read_csv('pwds.csv')
+    for row in hx:
+        print(row)
+
+def return_latest_pass():
+    hx = read_csv('pwds.csv')
+    return hx[-1]
+
+def set_password(password):
+    add_entry('pwds.csv',password)
+    print('Password added')
+    
+
+
